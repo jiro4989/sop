90ba4a854df31ca73cb577f25c2dab1e1ced2c7f
jiro4989 <jiroron666@gmail.com>
2018-12-08 10:51:20 +0900

    :sparkles:Vimを起動できるようにした

--- cli/command/command_edit.go
+++ cli/command/command_edit.go
@@ -1,8 +1,8 @@
 package command
 
 import (
-	"fmt"
 	"log"
+	"os"
 	"os/exec"
 
 	"github.com/spf13/cobra"
@@ -13,25 +13,33 @@ var editCommand = &cobra.Command{
 	Short: "edit",
 	Long:  "edit",
 	Run: func(cmd *cobra.Command, args []string) {
+		log.Println("start edit. args=", args)
 		if len(args) < 2 {
-			log.Println("need 1 args.")
+			log.Println("need 1 args.", args)
 			return
 		}
 
 		// if err := file.Backup(srcFile); err != nil {
 		// 	msg := fmt.Sprintf("failed backup. err=%s", err)
-		// 	log.Println(msg)
+		// 	log.Info(msg)
 		// 	return
 		// }
 
 		editor := args[0]
 		srcFile := args[1]
-		c := fmt.Sprintf("%s %s", editor, srcFile)
-		if err := exec.Command(c).Run(); err != nil {
-			log.Println("failed")
+		log.Println("editor=", editor, ", file=", srcFile)
+
+		c := exec.Command(editor, srcFile)
+		c.Stdout = os.Stdout
+		c.Stderr = os.Stderr
+		c.Stdin = os.Stdin
+
+		if err := c.Run(); err != nil {
+			log.Println("failed exec command. editor=", editor, ", srcFile-", srcFile, ", err=", err)
 			return
 		}
 
+		log.Println("end edit.")
 	},
 }
 
